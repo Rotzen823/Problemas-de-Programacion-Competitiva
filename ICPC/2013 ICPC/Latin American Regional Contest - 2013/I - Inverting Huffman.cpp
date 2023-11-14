f307b26055dae78ff163fe157c7867b8b84ab486
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2023-11-14 11:44:27 -0700

    Update I - Inverting Huffman.cpp

--- ICPC/2013 ICPC/Latin American Regional Contest - 2013/I - Inverting Huffman.cpp
+++ ICPC/2013 ICPC/Latin American Regional Contest - 2013/I - Inverting Huffman.cpp
@@ -9,8 +9,8 @@ typedef vector<ll> vi;
 int cont[MAX];
  
 int main(){
-    //ios_base::sync_with_stdio(0);
-    //cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     
     int n;
     cin >> n;
