2b2665041359b0088ce35e5eebde20934900e3ba
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2024-04-24 22:06:32 -0700

    Create B - Supporting everyone.cpp

---
+++ ICPC SWERC/2023-2024 ICPC Southwestern European Regional Contest (SWERC 2023)/B - Supporting everyone.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define MAX 105
+#define INF 1000000000
+
+using namespace std;
+// Pura Gente del Coach Moy
+using ll = long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<ii>;
+
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) ((int)(x).size())
+#define ALL(x) begin(x), end(x)
+#define FOR(i, a, b) for (int i = (int)a; i < (int)b; ++i)
+#define ROF(i, a, b) for (int i = (int)a - 1; i >= (int)b; --i)
+#define ENDL '\n'
+
+pair<int, vi> hopcroftKarp(vector<vi>& g, int m) {
+  int res = 0;
+  vi btoa(m, -1), A(SZ(g)), B(m), cur, next;
+  auto dfs = [&](auto self, int a, int L) -> bool {
+    if (A[a] != L) return 0;
+    A[a] = -1;
+    for (int b : g[a]) if (B[b] == L + 1) {
+      B[b] = 0;
+      if (btoa[b] == -1 || self(self, btoa[b], L + 1)) return btoa[b] = a, 1;
+    }
+    return 0;
+  };
+  while (1) {
+    fill(ALL(A), 0);
+    fill(ALL(B), 0);
+    /// Encuentra los nodos restantes para BFS (i.e. con layer 0)
+    cur.clear();
+    for (int a : btoa) if(a != -1) A[a] = -1;
+    FOR (a, 0, SZ(g)) if(A[a] == 0) cur.pb(a);
+    /// Encunetra todas las layers usando BFS
+    for (int lay = 1;; lay++) {
+      bool islast = 0;
+      next.clear();
+      for (int a : cur) for (int b : g[a]) {
+        if (btoa[b] == -1) {
+          B[b] = lay;
+          islast = 1;
+        }
+        else if (btoa[b] != a && !B[b]) {
+          B[b] = lay;
+          next.pb(btoa[b]);
+        }
+      }
+      if (islast) break;
+      if (next.empty()) return {res, btoa};
+      for (int a : next) A[a] = lay;
+      cur.swap(next);
+    }
+    /// Usa DFS para escanear caminos aumentantes
+    FOR (a, 0, SZ(g)) res += dfs(dfs, a, 0);
+  }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int n, m;
+	cin >> n >> m;
+
+	vector<vi> g(n, vi());
+	for(int i = 0; i < n; i++){
+		int k;
+		cin >> k;
+		while(k--){
+			int c;
+			cin >> c;
+			c--;
+			g[i].pb(c);
+		}
+	}
+
+	int ans = hopcroftKarp(g, m).fi;
+
+	cout << ans << ENDL;
+
+  return 0;
+}
