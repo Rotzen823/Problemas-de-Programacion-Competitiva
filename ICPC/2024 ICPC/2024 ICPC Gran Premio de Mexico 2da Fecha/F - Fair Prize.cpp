5ab633608e65f01fb633ae2ec63b846a888070e2
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2024-06-27 10:38:41 -0600

    Create F - Fair Prize.cpp

---
+++ ICPC/2024 ICPC/2024 ICPC Gran Premio de Mexico 2da Fecha/F - Fair Prize.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define MAX 1000005
+#define MOD 1000000007
+
+using namespace std;
+// Pura Gente del Coach Moy
+using ll = long long;
+using ii = pair<ll, ll>;
+using vi = vector<ll>;
+using vii = vector<ii>;
+
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) ((int)(x).size())
+#define ALL(x) begin(x), end(x)
+#define FOR(i, a, b) for (int i = (int)a; i < (int)b; ++i)
+#define ROF(i, a, b) for (int i = (int)a - 1; i >= (int)b; --i)
+#define ENDL '\n'
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int n, p;
+    cin >> n >> p;
+
+    int ans = 0;
+    while(n--){
+        int x;
+        cin >> x;
+        if(x <= p){
+            ans = max(ans, x);
+        }
+    }
+
+    cout << ans << ENDL;
+
+ 	return 0;
+}
