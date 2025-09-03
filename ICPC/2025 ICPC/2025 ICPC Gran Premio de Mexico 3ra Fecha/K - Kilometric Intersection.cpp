fc3abe107857d5eaa1113a9f7ae308356fb75994
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2025-09-03 11:34:08 -0600

    Create K - Kilometric Intersection.cpp

---
+++ ICPC/2025 ICPC/2025 ICPC Gran Premio de Mexico 3ra Fecha/K - Kilometric Intersection.cpp
@@ -0,0 +1,46 @@
+// Pura Gente del Coach Moy
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define pb push_back
+#define sz size
+#define all(x) x.begin(), x.end()
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define forns(i, s, n) for(int i = s; i < n; i++)
+#define ENDL '\n'
+#define INF 1000000000
+#define MOD 1000000007
+#define MAX 1005
+#define LOG 20
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        ll a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        if(c > b || d < a){
+            cout << "0\n";
+            continue;
+        }
+
+        ll ans = min(b, d) - max(a, c);
+        cout << ans << ENDL;
+    }
+
+    return 0;
+}
